66faed5709eed804c055d1e3707b396571f2a6ea
Robin Suter <suter.robin@gmail.com>
2017-12-04 11:14:45 +0100

    add decoder

--- affine_cipher.hs
+++ affine_cipher.hs
@@ -12,3 +12,13 @@ shift c a b | isLower c = int2let ((let2int c * a + b) `mod` 26)
 
 encode :: Int -> Int -> String -> String
 encode a b xs = [shift x a b | x <- xs]
+
+mod_inverse :: Int -> Int -> Int
+mod_inverse a m = [x | x <- [1..m], (a*x `mod` m) == 1] !! 0
+
+shift_back :: Char -> Int -> Int -> Char
+shift_back c a b | isLower c = int2let ((mod_inverse a 26) * (let2int c - b) `mod` 26)
+                 | otherwise = c
+
+decode :: Int -> Int -> String -> String
+decode a b xs = [shift_back x a b | x <- xs]
